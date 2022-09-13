d1503fef0fdc98af30a5fdb5fc62eb501da04d4c
YevheniiLi <96422034+YevheniiLi@users.noreply.github.com>
2022-09-13 15:12:34 +0000

    Deploying to gh-pages from @ YevheniiLi/goit-react-hw-05-movies@b6d4cc4c5be9ff9c4c9cd355b19a529f97660e94 🚀

---
+++ static/js/692.c364c170.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[692],{7692:function(t,e,a){a.d(e,{YiX:function(){return n}});var r=a(9983);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M21 11H6.414l5.293-5.293-1.414-1.414L2.586 12l7.707 7.707 1.414-1.414L6.414 13H21z"}}]})(t)}}}]);
+//# sourceMappingURL=692.c364c170.chunk.js.map
